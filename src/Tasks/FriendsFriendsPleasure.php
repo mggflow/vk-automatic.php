5cbd6f82dc3156bbb5c8b20a4a869adae822720e
Deusagnis <deusagnis@gmail.com>
2022-04-01 00:55:50 +0300

    Create FriendsFriendsPleasure.

---
+++ src/Tasks/FriendsFriendsPleasure.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MGGFLOW\VK\Automatic\Tasks;
+
+use MGGFLOW\VK\Automatic\API\GetRandFriendRandFriends;
+use MGGFLOW\VK\Automatic\API\LikeUserRandItem;
+use MGGFLOW\VK\Automatic\Interfaces\Task;
+
+class FriendsFriendsPleasure extends VKTask implements Task
+{
+
+    public function execute(): object
+    {
+        $result = (object)[
+            'friendFriendsResult' => false,
+            'likesResult' => [],
+        ];
+
+        $result->friendFriendsResult = GetRandFriendRandFriends::get(
+            $this->api,
+            $this->taskData->volume,
+        );
+        if (empty($result->friendFriendsResult->friendFriends)) return $result;
+
+        foreach ($result->friendFriendsResult->friendFriends as $userId){
+            $result['likesResult'][$userId] = LikeUserRandItem::like($this->api, $userId);
+        }
+
+        return $result;
+    }
+}
